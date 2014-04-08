9e323e7a1c2c12338e08da6c6f5c85fed8bdd358
Jordan Donais <jpdonais@tamu.edu>
2014-04-08 12:55:38 -0500

    New table
    New table for silly quotes. Just for fun.

---
+++ downloads/PokemonsParser/PokemonsParser/bin/Debug/sql_2_dbQuotes.sql
@@ -0,0 +1,24 @@
+CREATE TABLE RandomQuotes (
+	Message		CHAR(255) PRIMARY KEY
+);
+
+INSERT INTO RandomQuotes VALUES ('A good friend left me... and I miss her every day. But I know we''ll always be friends. Forever.');
+INSERT INTO RandomQuotes VALUES ('AHH! I wasted a donut!');
+INSERT INTO RandomQuotes VALUES ('Her temper could sure use a little evolution.');
+INSERT INTO RandomQuotes VALUES ('I believe in rock-hard defense and determination!');
+INSERT INTO RandomQuotes VALUES ('I came when I heard you defeated the Elite Four');
+INSERT INTO RandomQuotes VALUES ('I have my own method of bending spoons. (bends the spoon with his hands) Ha! Muscle over mind!');
+INSERT INTO RandomQuotes VALUES ('I heard there was a Cut Master on board, but he was just a seasick old man.');
+INSERT INTO RandomQuotes VALUES ('I''m having a major hat crisis. Could you try to steal Pikachu some other time?');
+INSERT INTO RandomQuotes VALUES ('I''m twice as good as Gary!');
+INSERT INTO RandomQuotes VALUES ('If Pikachu''s electric attacks don''t have effects, Ash is in trouble.');
+INSERT INTO RandomQuotes VALUES ('Isn''t that color a little bright for a ninja?');
+INSERT INTO RandomQuotes VALUES ('Pika Pika');
+INSERT INTO RandomQuotes VALUES ('Pikachu, I choose you!');
+INSERT INTO RandomQuotes VALUES ('Pokémon around the world wait for you, Ash.');
+INSERT INTO RandomQuotes VALUES ('Smell ya later.');
+INSERT INTO RandomQuotes VALUES ('TM 11 is Bubblebeam!');
+INSERT INTO RandomQuotes VALUES ('Treecko! Use your Tail Slap attack!');
+INSERT INTO RandomQuotes VALUES ('Uh, thanks! And I''ll also find out what rigorous means, too!');
+INSERT INTO RandomQuotes VALUES ('When you have lemons, you make lemonade; and when you have rice, you make rice balls.');
+INSERT INTO RandomQuotes VALUES ('You fools! You search for months and bring me this thing!? You are total incompetents!');
